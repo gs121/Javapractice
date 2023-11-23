2eea5de1609ba40374ab6871b028eab49b31b1ce
admin <admin@DESKTOP-OGPR9UG>
2023-11-23 12:53:38 +0530

    added use of multiple class

---
+++ binod/src/binod/Multiple2class.java
@@ -0,0 +1,7 @@
+package binod;
+
+public class Multiple2class {
+public void displaymovie() {
+	System.out.println("Dunki");
+}
+}
---
+++ binod/src/binod/Multiple_class.java
@@ -0,0 +1,13 @@
+package binod;
+
+public class Multiple_class {
+	public static void main(String args[])
+	{
+		
+		Multiple2class a = new Multiple2class();
+		a.displaymovie();
+System.out.print(a.toString());	}
+	
+	
+
+}
